625eba97261d97908fd2686cf45c775ec82a111c
Karl-Johan Karlsson <creideiki@ferretporn.se>
2025-03-04 20:50:13 +0100

    Adapt to document structure changes as of 2025-03-04

--- fa-notification-viewer/index.user.js
+++ fa-notification-viewer/index.user.js
@@ -20,8 +20,8 @@ if (window.location.pathname.startsWith('/msg/submissions')) {
 }
 
 function addStartViewerButton() {
-    let gallerySection = document.querySelector('.gallery-section');
-    let buttonsSection = gallerySection.querySelector('div.section-body:nth-child(2) > div:nth-child(1)');
+    let gallerySection = document.evaluate('//section[@class="gallery-section"][2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    let buttonsSection = gallerySection.querySelector('div.messagecenter-navigation');
     let submissionsAvailable = gallerySection.querySelector('.no-messages') === null;
 
     let startViewerButton = document.createElement('a');
