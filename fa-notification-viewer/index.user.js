6b38c49705963cf95640ee5bf194bc17a0b8d23f
Karl-Johan Karlsson <creideiki@ferretporn.se>
2026-05-10 00:07:29 +0200

    Adapt to document structure changes as of 2026-05-09
    The "+Fav" button has moved in the DOM tree.

--- fa-notification-viewer/index.user.js
+++ fa-notification-viewer/index.user.js
@@ -66,7 +66,7 @@ function resumeViewerSession() {
     let nextSubmissionId = sessionData.submissionIds[Math.min(submissionIndex + 1, sessionData.submissionIds.length)];
     let previousSubmissionId = sessionData.submissionIds[Math.max(submissionIndex - 1, 0)];
 
-    let favoriteActionUrl = document.querySelector('.submission-sidebar > .buttons > .fav > a').href;
+    let favoriteActionUrl = document.querySelector('.submission-controls-upper > a').href;
     let isAlreadyFavorite = favoriteActionUrl.includes('/unfav/');
 
     let notificationAlreadyRemoved = sessionData.removedIds.includes(idFromUrl);
